3854f6db973b74363732a4ea1f1cebdcef1bbb7f
CHILAKaNAVEEN <chnaveen7713@gmail.com>
2024-12-16 14:43:05 +0530

    first project

--- src/main/java/Bascclassesfrontend/Searchhospitalbaseclass.java
+++ src/main/java/Bascclassesfrontend/Searchhospitalbaseclass.java
@@ -1,12 +1,5 @@
 package Bascclassesfrontend;
 
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-
 public class Searchhospitalbaseclass {
 
 	
--- src/main/java/utilsactionpages/HotelBookingTest.java
+++ src/main/java/utilsactionpages/HotelBookingTest.java
@@ -4,10 +4,11 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.testng.Assert;
+//import org.testng.annotations.AfterClass;
+//import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import pagesinProject.BookingPage;
